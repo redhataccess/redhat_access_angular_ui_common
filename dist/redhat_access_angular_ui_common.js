716ae38d01fdb2f38a7930323168ef4bfe01b00b
Jonáš Trantina <jtrantin@redhat.com>
2016-04-07 17:51:39 +0200

    PCM-1915: Added Wapps URL

--- dist/redhat_access_angular_ui_common.js
+++ dist/redhat_access_angular_ui_common.js
@@ -1,4 +1,4 @@
-/*! redhat_access_angular_ui_common - v1.1.33 - 2016-04-06
+/*! redhat_access_angular_ui_common - v1.1.34 - 2016-04-07
  * Copyright (c) 2016 ;
  * Licensed 
  */
@@ -923,6 +923,24 @@ angular.module('RedhatAccess.common').service('ConstantsService', [
                 value: STATUS.closed
             }
         ];
+
+
+        this.wappsUrl = new Uri('https://ams-dev2.devlab.redhat.com/wapps');
+        if (window.location.hostname === 'access.redhat.com' || window.location.hostname === 'prod.foo.redhat.com' || window.location.hostname === 'fooprod.redhat.com'){
+            this.wappsUrl = new Uri('https://www.redhat.com/wapps');
+        }
+        else
+        {
+            if (window.location.hostname === 'access.qa.redhat.com' || window.location.hostname === 'qa.foo.redhat.com' || window.location.hostname === 'fooqa.redhat.com') {
+                this.wappsUrl = new Uri('https://www.qa.redhat.com/wapps');
+            }
+            else
+            {
+                if (window.location.hostname === 'access.devgssci.devlab.phx1.redhat.com' || window.location.hostname === 'ci.foo.redhat.com' || window.location.hostname === 'fooci.redhat.com') {
+                    this.wappsUrl = new Uri('https://ams-dev2.devlab.redhat.com/wapps');
+                }
+            }
+        }
     }
 ]);
 
